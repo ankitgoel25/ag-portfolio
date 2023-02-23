84a2b2829346f6d0bdeadb903a0733c10d91a1d1
ankitgoel25 <ankitoct01@gmail.com>
2023-02-23 15:21:50 +0530

    Initial Setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import PropTypes from 'prop-types';
+import '@/styles/globals.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}),
+};
